9b633f6c8d992ef5800f83369f91de66d6e99c70
KrTvGG <vanyusha.krutov2@bk.ru>
2024-10-08 16:22:56 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup>
+    const {data} = useFetch('https://apa-auto.ru/wp-json/wp/v2/car');
+</script>
+<template>
+    <div class="item" v-for="item in data">
+        {{item.title.rendered}}
+    </div>
+</template>
