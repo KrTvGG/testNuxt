0f5ca896e23f27aea19dea643abec08effd9bd36
ps_krutov_i <krutov.ivan@place-start.ru>
2024-10-08 19:48:58 +0300

    пагинация

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <script setup>
-    const {data} = useFetch('https://apa-auto.ru/wp-json/wp/v2/car');
+    const {data} = useFetch('https://apa-auto.ru/wp-json/wp/v2/car?per_page=100')
+    const page = 1
 </script>
 <template>
     <div class="item" v-for="item in data">
         {{item.title.rendered}}
     </div>
+    <UPagination :total="data.length" :model-value="1" :page-count="5"/>
 </template>
